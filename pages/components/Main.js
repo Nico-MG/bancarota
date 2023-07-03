ee2f2f01c9e274fcdf2cf81f07de365c66e9ac45
A. Cid <lordix.acid@gmail.com>
2023-07-03 14:25:09 -0400

    Mensaje cuando no hay resultados de busqueda

--- pages/components/Main.js
+++ pages/components/Main.js
@@ -1,11 +1,40 @@
+import { useEffect, useState } from "react";
 import InformationTable from "./InformationTable";
 import Nav from "./Nav";
+import Image from "next/image";
 
 export default function Main({ data, changeUrlFetch, pager }) {
+    const [results, setResults] = useState(true);
+
+    useEffect(() => {
+        if (data) {
+            setResults(data.length > 0);
+        }
+    }, [data]);
+
     return (
         <main className="main">
             <Nav changeUrlFetch={changeUrlFetch} />
-            <InformationTable data={data} pager={pager} changeUrlFetch={changeUrlFetch} />
+
+            {results ? (
+                <InformationTable
+                    data={data}
+                    pager={pager}
+                    changeUrlFetch={changeUrlFetch}
+                />
+            ) : (
+                <div className="search__result_message">
+                    <Image
+                        src="/desert.svg"
+                        priority={false}
+                        width={300}
+                        height={300}
+                        style={{ borderRadius: "100%", opacity: 0.9 }}
+                        alt="No results"
+                    />
+                    No hay resultados para esta busqueda...
+                </div>
+            )}
         </main>
     );
 }
